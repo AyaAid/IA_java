1861a26241b936d81ac6737cb0c3f9254ba86243
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2023-01-23 12:17:47 +0100

    Menu

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) throws Exception{
+        afficherMenu();
     }
+    private static Scanner _scan;
+
+    static {
+        _scan = new Scanner(System.in);
+    }
+
+    public App() {
+    }
+    
+private static void afficherMenu() {
+    ArrayList<String> menus = new ArrayList<>();
+    menus.add("          ⥤ MENU ⥢");
+    menus.add("1-   ⌈     Jouez !      ⌋");
+    menus.add("2-   ⌈    Classement    ⌋");
+    menus.add("3-   ⌈Choix des couleurs⌋");
+    menus.add("4-   ⌈    Difficulté    ⌋");
+    menus.add("q-   ⌈     Quitter      ⌋");
+    Iterator<String> iterator = menus.iterator();
+
+    while(iterator.hasNext()) {
+        String menu = iterator.next();
+        System.out.println(menu);
+    }
+}
 }
