66221c6343504c291b210b54e7d54cbd71af6ef1
AyaAid <aya.lakhel11@gmail.com>
2023-01-23 12:17:10 +0100

    joueur

---
+++ joueur/Joueur.java
@@ -0,0 +1,38 @@
+package joueur;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Joueur {
+
+    private String joueur;
+    private String couleur;
+
+    public joueur(String joueur, String couleur) {
+        this.joueur = joueur;
+        this.couleur = couleur;
+    }
+
+    public String getJoueur() {
+        return joueur;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+   
+    public void enregistrer()throws IOException {
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("classement.csv", true)));
+        try{
+            pw.println(joueur);
+        } finally {
+            pw.close();
+        }
+    }
+
+
+    
+}
