619c716d414b4a9e7c3eba7f9d25035b3336bc99
AyaAid <aya.lakhel11@gmail.com>
2023-01-23 20:42:51 +0100

    une grille encore mieux

--- src/model/Grille.java
+++ src/model/Grille.java
@@ -4,28 +4,29 @@ import java.util.*;
 
 
 public class Grille {
-    private int rows = 7;
-    private int columns = 6;
-    ArrayList<ArrayList<String>> grid = new ArrayList<ArrayList<String>>();
+    private static int rows = 7;
+    private static int columns = 6;
+    static ArrayList<ArrayList<String>> grid = new ArrayList<ArrayList<String>>();
 
-    public void afficherGrille(){
+
+    public static void afficherGrille(){
         for (int i = 0; i < rows; i++) {
             grid.add(new ArrayList<String>(columns));
             }
         System.out.println("   1     2     3     4     5     6     7");
+        System.out.println();
         for(int c = 1; c <= columns; c++){
-            System.out.println(c + "  ");
+            System.out.println("  ");
+            for(int i= 0; i < rows; i++){
+
+                    System.out.print("|----|");
+                
+            }
+            System.out.println();
         }
         System.out.println();
 
-        for(int i= 0; i < grid.size(); i++){
-            for(int j = 0; j < grid.get(i).size(); j++){
-                System.out.print("|----|");
-            }    
-            System.out.println();
-            }
-
-        }
+    }
     
     public boolean grillePleine() {
         for (int i = 0; i < grid.size(); i++) {
