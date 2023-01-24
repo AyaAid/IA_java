2b2339a5c81b2fdc3cab53f5b81509e52c953bc4
Ramjoh <romjah@orange.fr>
2023-01-24 11:56:03 +0100

    app

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -13,18 +14,13 @@ public class App {
 
     public static void main(String[] args) throws Exception{
         while (true) {
-            afficherMenu();
+            Menu.afficherMenu("principale");
             switch (_scan.nextLine()) {
                 case "1":
                         System.out.println("\nVous avez choisi de jouer en solo\n");
-                        SelectionCouleur();
-                        SelectionSymbole();
-                        NomduJoueurSolo();
                     return;
                 case "2":
-                        SelectionCouleur();
-                        SelectionSymbole();
-                        NomduJoueur();
+                        System.out.println("\nVous avez choisi de jouer contre l'IA\n");
                     return;
                 case "3":
                         System.out.println("Vous avez choisi de voir le classement");
@@ -46,19 +42,5 @@ public class App {
     public App() {
     }
     
-    public static void afficherMenu() {
-        ArrayList<String> menus = new ArrayList<>();
-        menus.add("          \u001B[4m⥤ MENU ⥢\u001B[0m\n");
-        menus.add("\u001B[32m1-   ⌈    Jouer solo !  ⌋\u001B[0m");
-        menus.add("\u001B[93m2-   ⌈   Jouer à deux   ⌋\u001B[0m");
-        menus.add("\u001B[33m3-   ⌈    Classement    ⌋\u001B[0m");
-        menus.add("\u001B[35m4-   ⌈     Quitter      ⌋\u001B[0m\n");
-        Iterator<String> iterator = menus.iterator();
-
-        while(iterator.hasNext()) {
-            String menu = iterator.next();
-            System.out.println(menu);
-        }
-        System.out.println("Veuillez choisir une option :");
-    }
+   
 } 
