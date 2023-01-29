6cdcfde5be25eb95e6d30d6b86a84bc1e3c4af37
AyaAid <aya.lakhel11@gmail.com>
2023-01-29 16:01:56 +0100

    last push joueur

--- src/model/Joueur.java
+++ src/model/Joueur.java
@@ -3,37 +3,23 @@ package model;
 public class Joueur {
 
     /**
-     *@autor : Aya et Romain
-     * @return
-     * Cette classe permet de définir les joueurs et leurs couleurs.
-     */
+     *@author Aya
+    */
+
     private String joueur;
     private String couleur;
 
-    /**
-     * @param joueur
-     * @param couleur
-     * @return 
-     * Cette méthode permet de définir les joueurs et leurs couleurs.
-     */
-    public Joueur(String joueur, String couleur) { // Cette ligne déclare un constructeur qui prend en paramètre le nom du joueur et la couleur et les stocke dans les variables déclarées précédemment.
+
+    public Joueur(String joueur, String couleur) {
 
         this.joueur = joueur;
         this.couleur = couleur;
     }
 
-    /**
-     * @return
-     * Cette méthode permet de récupérer le nom du joueur.
-     */
     public String getJoueur() {
         return joueur;
     }
 
-    /**
-     * @return
-     * Cette méthode permet de récupérer la couleur du joueur.
-     */
     public String getCouleur() {
         return couleur;
         
